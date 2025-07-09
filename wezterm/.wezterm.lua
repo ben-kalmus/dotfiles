d25b2389d1d91830bf78ab4737f304b04cf3d357
Ben Kalmus <ben.kalmus@algolia.com>
2025-07-09 11:18:50 +0100

    change wezterm opacity

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -12,7 +12,7 @@ config.initial_rows = 48
 config.font_size = 13
 config.font = wezterm.font("Fira Code")
 config.color_scheme = "Tokyo Night Moon"
-config.window_background_opacity = 0.95
+config.window_background_opacity = 0.93
 
 config.term = "xterm-256color"
 
